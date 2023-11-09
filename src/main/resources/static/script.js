eb41014ff3370c4f529c9cb20ab4d6f2369f7943
mamish12 <mku20sahu@gmail.com>
2023-11-09 16:17:45 +0530

    first code

---
+++ src/main/resources/static/script.js
@@ -0,0 +1,29 @@
+var stompClient = null
+
+function connect() {
+	let socket = new SockJS("/server1")
+	stompClient = Stomp.over(socket)
+
+	stompClient.connect({}, function(frame) {
+		
+		console.log("Connected: "+frame)
+
+
+	})
+
+}
+
+  
+$(document).ready((e) => {
+
+	$("#login").click(() => {
+
+		let name = $("#name-value").val()
+		localStorage.setItem("name",name);
+
+
+
+		connect();
+	})
+
+})
